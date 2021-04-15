4e7a1c486b781a1cd7d89932745e924244bd782b
Joknaa <54961980+Joknaa@users.noreply.github.com>
2021-04-15 01:36:00 +0000

    Added new walls to the Tileset
    I created a new terrain; ground, walls, and ceiling tiles.

---
+++ IngDow/Assets/Art/.Inspiration/Pattern-Panic-10x10/README.txt
@@ -0,0 +1,16 @@
+Pattern Panic
+-------------
+
+Enjoy my assets? Keep up to date by following me on itch or twitter.
+
+https://v3x3d.itch.io/
+https://twitter.com/_V3X3D
+
+You can also support me on Patreon for just a $1 each month.
+Your support helps me keep making assets and provides you rewards.
+
+https://www.patreon.com/V3X3D
+
+
+
+-- VEXED
--- IngDow/Assets/SuperTiled2Unity/Import/Dow_TileSet.tsx
+++ IngDow/Assets/SuperTiled2Unity/Import/Dow_TileSet.tsx
@@ -2,8 +2,9 @@
 <tileset version="1.4" tiledversion="1.4.3" name="Dow_TileSet" tilewidth="16" tileheight="16" tilecount="400" columns="20">
  <image source="TileSet/Dow_TileSet.png" width="320" height="320"/>
  <terraintypes>
-  <terrain name="Walls" tile="-1"/>
+  <terrain name="Walls 01" tile="21"/>
   <terrain name="LockedDoor" tile="-1"/>
+  <terrain name="Wall 02" tile="369"/>
  </terraintypes>
  <tile id="16" terrain=",,,1"/>
  <tile id="17" terrain=",,1,"/>
@@ -24,4 +25,29 @@
  <tile id="60" terrain=",0,,"/>
  <tile id="61" terrain="0,0,,"/>
  <tile id="62" terrain="0,,,"/>
+ <tile id="231" terrain="2,2,2,2"/>
+ <tile id="248" terrain="2,2,2,"/>
+ <tile id="249" terrain="2,2,,"/>
+ <tile id="250" terrain="2,2,,"/>
+ <tile id="251" terrain="2,2,,"/>
+ <tile id="252" terrain="2,2,,2"/>
+ <tile id="268" terrain="2,,2,"/>
+ <tile id="272" terrain=",2,,2"/>
+ <tile id="288" terrain="2,,2,2"/>
+ <tile id="289" terrain=",,2,"/>
+ <tile id="291" terrain=",,,2"/>
+ <tile id="292" terrain=",2,2,2"/>
+ <tile id="309" terrain="2,,2,"/>
+ <tile id="311" terrain=",2,,2"/>
+ <tile id="328" terrain="2,2,2,"/>
+ <tile id="329" terrain="2,,,"/>
+ <tile id="331" terrain=",2,,"/>
+ <tile id="332" terrain="2,2,,2"/>
+ <tile id="348" terrain="2,,2,"/>
+ <tile id="352" terrain=",2,,2"/>
+ <tile id="368" terrain="2,,2,2"/>
+ <tile id="369" terrain=",,2,2"/>
+ <tile id="370" terrain=",,2,2"/>
+ <tile id="371" terrain=",,2,2"/>
+ <tile id="372" terrain=",2,2,2"/>
 </tileset>
